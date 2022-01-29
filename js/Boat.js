e93f526ad5d25d0296497c435e1bb63589273122
ApurvaChandrakar2301 <89448650+ApurvaChandrakar2301@users.noreply.github.com>
2022-01-29 13:07:56 +0530

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,26 @@
+class Boat{
+    constructor(x,y,w,h,boatPos){
+        var options={
+            restitution:0.6,
+            friction:1.0,
+            density:1.0
+        }
+        this.body=Bodies.rectangle(x,y,w,h);
+        this.w=w;
+        this.h=h;
+        this.image=loadImage("assets/boat.png")
+        this.boatPosition=boatPos
+        World .add(world,this.body)
+
+    }
+    display(){
+
+        var pos= this.body.position
+        push() 
+        translate(pos.x,pos.y)
+        imageMode(CENTER)
+        image(this.image,0,this.boatPosition,this.w,this.h)                
+        pop ()
+
+   }
+}
